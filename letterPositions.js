30828a908a286b51d556e8f67e34174af9c6fda8
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-01-08 21:56:51 +0000

    Created a function letterPositions which returns all the indices in a string where each character is found

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+const eqArrays = (arr1, arr2)=> {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        // do nothing
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = (arr1, arr2)=> {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  let index = 0;
+  
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter].push(index);
+      } else {
+        results[letter] = [index];
+      }
+    }
+    index++;
+  }
+  return results;
+};
+
+console.log(letterPositions('hello'));
+console.log(letterPositions('lighthouse in the house'));
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+
