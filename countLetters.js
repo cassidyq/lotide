6e5aeff623f631da6a804fc2ee108737d2fe4720
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-01-08 21:19:17 +0000

    Created a function that returns the counts of each letter in a sentence

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let results = {};
+
+  for (const letter of sentence) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+let args = process.argv.slice(2);
+let sentence = args.join('');
+
+console.log(countLetters(sentence));
+
+assertEqual(countLetters('lighthouseinthehouse')['i'], 2);
