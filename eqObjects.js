e48b7051a9ab3cbd6edf7211d5b63843f6ade4fc
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-01-11 02:49:00 +0000

    updated eqObjects function to use recursion

--- eqObjects.js
+++ eqObjects.js
@@ -6,43 +6,28 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = (arr1, arr2)=> {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        // do nothing
-      } else {
-        return false;
-      }
-    }
-    return true;
-  }
-};
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   let keys1 = Object.keys(object1).sort();
-  let keys2 = Object.keys(object2).sort();
+  let keys2 = Object.keys(object2).sort()
 
   if (keys1.length !== keys2.length) {
-    return false;
-  } else if (eqArrays(keys1, keys2)) {
+    return false; // works
+  } 
     for (const key of keys1) {
       
-      if (Array.isArray(object1[key]) && eqArrays(object1[key], object2[key])) {
-        return true;
-      }
-      if (object1[key] !== object2[key]) {
+      if (typeof object1[key] === 'object' || Array.isArray(object1[key])) {
+        
+        if(!eqObjects(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
         return false;
       }
     }
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 };
 
 const ab = { a: "1", b: "2" };
@@ -64,3 +49,7 @@ assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
