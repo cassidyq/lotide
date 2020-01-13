a0fb4cb2a05c591ab148ed8958a35256ed72cb35
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-01-13 22:18:20 +0000

    Refactored eqArrays to use mocha chai tests and added function to lotide index

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,13 +1,48 @@
-const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+describe("#eqArrays", () => {
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+  it("should return true when given two equal arrays [1, 2, 3] and [1, 2, 3]", () => {
+    const result = eqArrays([1, 2, 3], [1, 2, 3]);
+    assert.strictEqual(result, true);
+  });
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+  it("should return false when given two arrays with same values in different order", () => {
+    const result = eqArrays([1, 2, 3], [3, 2, 1]);
+    assert.strictEqual(result, false);
+  });
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  it("should return false when given two arrays with same values but of different type ['1', '2', '3'], ['1', '2', 3]", () => {
+    const result = eqArrays(["1", "2", "3"], ["1", "2", 3]);
+    assert.strictEqual(result, false);
+  });
+  
+  it("should return false when given two arrays of different length", () => {
+    const result = eqArrays([1, 2, 3], [1, 2, 3, 4]);
+    assert.strictEqual(result, false);
+  });
+
+  it("should return true when given two matching arrays of arrays [[2, 3], [4]] and [[2, 3], [4]]", () => {
+    const result = eqArrays([[2, 3], [4]], [[2, 3], [4]]);
+    assert.strictEqual(result, true);
+  });
+ 
+  it("should return false when given two different arrays of arrays [[2, 3], [4]] and [[2, 3], [4, 5]]", () => {
+    const result = eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]);
+    assert.strictEqual(result, false);
+  });
+  
+});
+
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
