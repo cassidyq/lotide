abf0afebbbdd8be99fad6c746eee8c47a4f942dc
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-01-13 22:38:18 +0000

    added eqObjects to lotide index

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects')
 
 module.exports = {
   head,
   tail,
   middle,
-  eqArrays
+  eqArrays,
+  eqObjects
 };
