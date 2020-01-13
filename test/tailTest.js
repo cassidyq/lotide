1edf7c938a095b1e94240b2b5e1b7e2c467a8ffc
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-01-13 20:43:32 +0000

    updated tailTest.js to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,37 @@
-const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays');
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+const assert = require('chai').assert;
 
-// Test Case 2: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!\
+describe("#tail", () => {
+  it("should return everything but the head", () => {
+    const result2 = tail([1, 2, 3, 4, 5]);
+    assert.deepEqual(result2, [2, 3, 4, 5]);
+  });
 
-//Test Case 3: Check the returned array
-const result2 = tail([1, 2, 3, 4, 5]);
-assertEqual(eqArrays(result2, [2, 3, 4, 5]), true);
+  it("should return array length 2 when given length 3", () => {
+    const result2 = tail([1, 2, 3]);
+    assert.equal(result2.length, 2);
+  });
+  
+  it("should not modify the original array after calling it with tail", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.deepEqual(["Yo Yo", "Lighthouse", "Labs"], words);
+  });
+  
+});
+
+// // Test Case 1: Check the returned array elements
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// // Test Case 2: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!\
+
+// //Test Case 3: Check the returned array
+// const result2 = tail([1, 2, 3, 4, 5]);
+// assertEqual(eqArrays(result2, [2, 3, 4, 5]), true);
